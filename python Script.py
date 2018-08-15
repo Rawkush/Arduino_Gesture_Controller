b0ba6783ee68a005ecd1177c3657581993e578b2
rawkush <ankushrawat950@gmail.com>
2018-08-15 19:14:40 +0530

    creating serial port for ommunication

--- python Script.py
+++ python Script.py
@@ -2,3 +2,6 @@
 import serial #Serial imported for Serial communication
 import time #Required to use delay functionss
 import pyautogui
+
+ArduinoSerial = serial.Serial('com18',9600) #Create Serial port object called arduinoSerialData
+time.sleep(2) #wait for 2 seconds for the communication to get established
