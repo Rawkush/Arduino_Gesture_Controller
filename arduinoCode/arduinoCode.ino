fe3f16f9db5015180edc1773731525fcd45ba293
rawkush <ankushrawat950@gmail.com>
2018-08-15 18:59:55 +0530

    calcualting the distance

--- arduinoCode/arduinoCode.ino
+++ arduinoCode/arduinoCode.ino
@@ -5,10 +5,27 @@ const int echo1 = 3; //Echo pin of 1st Sesnor
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
-
+pinMode(trigger1, OUTPUT);
+pinMode(echo1, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+void calculateDistance()(int trigger,int echo){
+
+  
+}
+
+void sendAndTrigger(){
+
+digitalWrite(trigger, LOW);
+delayMicroseconds(2);
+digitalWrite(trigger, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigger, LOW);
+}
+
