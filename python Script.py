17e61ae9bfcc7490bfdf34e594619bcefbeccb8a
rawkush <ankushrawat950@gmail.com>
2018-08-15 19:19:29 +0530

    Added comments

--- python Script.py
+++ python Script.py
@@ -1,7 +1,7 @@
 
 import serial #Serial imported for Serial communication
 import time #Required to use delay functionss
-import pyautogui
+import pyautogui#required for autmating the input in behalf of the user
 
 ArduinoSerial = serial.Serial('com18',9600) #Create Serial port object called arduinoSerialData
 time.sleep(2) #wait for 2 seconds for the communication to get established
