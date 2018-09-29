0e5a469c889c64f2ebb6c299d3649f778dd2224b
rawkush <ankushrawat950@gmail.com>
2018-09-29 22:35:53 +0530

    few edits

--- python Script.py
+++ python Script.py
@@ -3,11 +3,11 @@ import serial #Serial imported for Serial communication
 import time #Required to use delay functionss
 import pyautogui#required for autmating the input in behalf of the user
 
-ArduinoSerial = serial.Serial('com18',9600) #Create Serial port object called arduinoSerialData
+ArduinoSerial = serial.Serial('COM4',9600) #Create Serial port object called arduinoSerialData
 time.sleep(2) #wait for 2 seconds for the communication to get established
-while 1:
+while True:
     incoming = str (ArduinoSerial.readline()) #read the serial data and print it as line
-    print incoming
+    print(incoming)
     
     if 'Play/Pause' in incoming:
         pyautogui.typewrite(['space'], 0.2)
